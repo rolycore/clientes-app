2955747db84ed0b97eeaee6f441dc51128eeaf2a
rolycore <rolycore@gmail.com>
2023-07-04 11:16:24 -0500

    Creando el proyecto en el repositorio

---
+++ src/main/java/com/sstproyects/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.sstproyects.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sstproyects.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+
+}
---
+++ src/main/java/com/sstproyects/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,13 @@
+package com.sstproyects.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.sstproyects.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
+	public Cliente findById(Long id);
+	public Cliente save(Cliente cliente);
+	public void delete(Long id);
+	
+}
